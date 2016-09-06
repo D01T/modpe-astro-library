b2b605797900188f0af254af32d69181452d3eb7
Astro36 <astr36@naver.com>
2016-09-06 19:05:11 +0900

    Add {ScrollView} class which can change its thumb and track

--- AstroLibrary.js
+++ AstroLibrary.js
@@ -2207,14 +2207,10 @@ let me = this.me || {};
      * @returns {android.widget.LinearLayout} Layout
      */
     Layout.prototype.show = function (canScroll) {
-        if (typeof canScroll === "undefined" || canScroll) {
-            let scrollView = new ScrollView_(CONTEXT);
+        if (this._layout.getOrientation() === 1 && (typeof canScroll === "undefined" || canScroll)) {
+            let scrollView = new ScrollView();
             scrollView.addView(this._layout);
-            
-scrollView.setVerticalScrollBarEnabled(false); 
-scrollView.setHorizontalScrollBarEnabled(false); 
-scrollView.setOverScrollMode(View_.OVER_SCROLL_NEVER);
-            return scrollView;
+            return scrollView.show();
         } else {
             return this._layout;
         }
@@ -2386,6 +2382,127 @@ scrollView.setOverScrollMode(View_.OVER_SCROLL_NEVER);
     ProgressWindow.prototype.stop = function () {
         this._isRunning = false;
         return this;
+    };
+
+
+
+    /**
+     * Class representing a scroll view.
+     * @since 2016-09-06
+     * @class
+     * @memberOf me.astro.widget
+     * @param {me.astro.design.Theme} [theme=me.astro.design.Theme.DEFAULT] Theme of scroll view
+     */
+    function ScrollView(theme) {
+        theme = theme || Theme.DEFAULT;
+        let thiz = this;
+        this._scrollView = new ScrollView_(CONTEXT);
+        this._scroller = new LinearLayout_(CONTEXT);
+        this._track = new TextView_(CONTEXT);
+        this._thumb = new TextView_(CONTEXT);
+        this._theme = theme;
+        this._width = DP * 340;
+        this._scrollView.setLayoutParams(new LinearLayout_.LayoutParams(this._width - DP * 6, -1));
+        this._scrollView.setOnScrollChangeListener(new View_.OnScrollChangeListener({
+            onScrollChange(v, scrollX, scrollY, oldScrollX, oldScrollY) {
+                CONTEXT.runOnUiThread({
+                    run() {
+                        thiz._track.setLayoutParams(new LinearLayout_.LayoutParams(DP * 12, v.getHeight() * scrollY / v.getChildAt(0).getHeight()));
+                    }
+                });
+            }
+        }));
+        this._scrollView.setOverScrollMode(View_.OVER_SCROLL_NEVER);
+        this._scrollView.setVerticalScrollBarEnabled(false);
+        this._scrollView.setHorizontalScrollBarEnabled(false);
+        this._scroller.addView(this._track);
+        this._scroller.addView(this._thumb);
+        this._scroller.setOrientation(1);
+        this._track.setLayoutParams(new LinearLayout_.LayoutParams(DP * 6, 0));
+        this._thumb.setBackgroundDrawable(new ColorDrawable_(theme.getProgressWindow(Theme.EFFECT_COLOR)));
+        this._thumb.setLayoutParams(new LinearLayout_.LayoutParams(DP * 6, DP * 36));
+        this._thumb.setOnTouchListener(new View_.OnTouchListener({
+            onTouch(v, event) {
+                let y = event.getRawY();
+                if (event.getAction() === MotionEvent_.ACTION_MOVE) {
+                    thiz._track.setLayoutParams(new LinearLayout_.LayoutParams(DP * 12, thiz._scrollView.getChildAt(0).getHeight() * y / (thiz._scrollView.getHeight() - DP * 18)));
+                    thiz._scrollView.scrollTo(0, thiz._scrollView.getChildAt(0).getHeight() * y / (thiz._scrollView.getHeight() - DP * 18));
+                }
+                return true;
+            }
+        }));
+    }
+
+    /**
+     * Adds a view on the scroll view.
+     * @since 2016-09-06
+     * @param {android.view.View} view View
+     */
+    ScrollView.prototype.addView = function (view) {
+        this._scrollView.addView(view);
+        return this;
+    };
+
+    /**
+     * Returns the scroll view.
+     * @since 2016-09-06
+     * @returns {android.widget.LinearLayout} Scroll view
+     */
+    ScrollView.prototype.getLayout = function () {
+        return this._layout;
+    };
+
+    /**
+     * Sets the color of the thumb of the scroll view.
+     * @since 2016-09-06
+     * @param {Number} color Color of the thumb of the scroll view
+     */
+    ScrollView.prototype.setThumbColor = function (color) {
+        this._thumb.setBackgroundDrawable(new ColorDrawable_(color));
+        return this;
+    };
+
+    /**
+     * Sets the drawable of the thumb of the scroll view.
+     * @since 2016-09-06
+     * @param {android.graphics.drawable.Drawable} drawable Drawable of the thumb of the scroll view
+     */
+    ScrollView.prototype.setThumbDrawable = function (drawable) {
+        this._thumb.setBackgroundDrawable(drawable);
+        return this;
+    };
+
+    /**
+     * Sets the color of the track of the scroll view.
+     * @since 2016-09-06
+     * @param {Number} color Color of the track of the scroll view
+     */
+    ScrollView.prototype.setTrackColor = function (color) {
+        this._track.setBackgroundDrawable(new ColorDrawable_(color));
+        return this;
+    };
+
+    /**
+     * Sets the drawable of the track of the scroll view.
+     * @since 2016-09-06
+     * @param {android.graphics.drawable.Drawable} drawable Drawable of the track of the scroll view
+     */
+    ScrollView.prototype.setTrackDrawable = function (drawable) {
+        this._track.setBackgroundDrawable(drawable);
+        return this;
+    };
+
+    /**
+     * Returns the scroll view.
+     * @since 2016-09-06
+     * @returns {android.widget.LinearLayout} Scroll view
+     */
+    ScrollView.prototype.show = function () {
+        let mainLayout = new LinearLayout_(CONTEXT);
+        mainLayout.addView(this._scrollView);
+        mainLayout.addView(this._scroller);
+        mainLayout.setLayoutParams(new LinearLayout_.LayoutParams(this._width, -1));
+        return mainLayout;
     };
 
 
